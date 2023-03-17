c8653dd369c3b4d481d7af216e57af42275394f4
Gopibabu Srungavarapu <gsrungavarapu@tmhcc.com>
2023-03-17 08:59:31 -0400

    📦 NEW: PHP: Added docker config and sample routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome', function () {
+    return 'Hello World';
+});
+
+Route::get('/greet', function () {
+    return 'Hello World greeting';
+});
+
+//Route::redirect('/welcome', '/greet', 301);
+Route::permanentRedirect('/welcome', '/greet');
