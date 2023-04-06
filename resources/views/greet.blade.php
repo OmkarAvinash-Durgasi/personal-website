49849f2c75b3f60b8925c8379922d501e6bfb9a2
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-04-06 07:17:16 -0400

    Created greeting file

--- resources/views/greet.blade.php
+++ resources/views/greet.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Larvel</title>
+    <title>My First Webpage</title>
 </head>
 <body>
 <h1> Welcome to Laravel with Omkar</h1>
