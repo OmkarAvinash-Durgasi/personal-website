7f5e2a19790aaccdfab6ce3d42567e336c96da4a
OMKARAVINASH\durga <durgasiomkaravinash@gmail.com>
2023-04-05 23:02:50 -0400

    Updated Home Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,14 +9,6 @@ class HomeController extends Controller
 {
     public function index(): View
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        return view('Welcome');
-=======
         return view('welcome');
->>>>>>> 8ee643196e28ba1f29c3d7cc2de4588127c999ec
-=======
-        return view('welcome');
->>>>>>> 8ee643196e28ba1f29c3d7cc2de4588127c999ec
     }
 }
